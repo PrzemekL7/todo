3031974998a42d1bf21ffde2525c71f0f1c3bcbc
PrzemekL7 <plehr7@gmail.com>
2022-03-24 15:47:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import {useState} from "react";
 
-function* uiid_gen() {
+function* uuidGen() {
   let id = 0;
 
-  while (true) {
+  while (true){
     yield id;
-    id++
+    id++;
   }
 }
 
+const uuid = uuidGen();
+
 function App() {
   const [value, setValue] = useState('');
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTask] = useState([]);
 
   const handleChange = (event) => {
-    setValue(event.target.value);
+    setValue(event.target.value)
   }
 
   const handleKeyUp = (event) => {
-    if (event.key === 'Enter'){
-      setTasks([...tasks,
-              name: value
-              id: uiid_gen.next().value]);
-      setValue('');
+    if (value !== ''){
+      if (event.key === 'Enter'){
+        setTask([...tasks, {
+          name: value,
+          id: uuid.next().value,
+          status: false
+        }])
+        // setTask([value, ...tasks]) //jesli chcemy na poczatku
+        setValue('')
+      }
     }
+  }
+
+  function handleChangeStatus(id) {
+    const newTasks = tasks.map(task => {
+      if (task.id === id){
+        task.status = !task.status
+      }
+      return task
+    })
+
+    setTask(newTasks);
+  }
+
+  function handleDeleteTask(id) {
+    setTask(tasks.filter(task => task.id !== id))
   }
 
   return (
     <div className="App">
-      <h1>todo</h1>
-      <input type="text" value={value}onChange={handleChange} onKeyUp={handleKeyUp}/>
+      <h1>todos</h1>
+      <input type="text"
+       value={value}
+       onChange={handleChange}
+       onKeyUp={handleKeyUp}
+       />
       <ul>
-        {tasks.map((item) => <li>{item}</li>)}
+        {tasks.map(({id, name, status}) => (
+          <li key={id} className='todo-item'>
+            <span
+              className={status ? 'status done' : 'status active'}
+              onClick={() => handleChangeStatus(id)}
+            />
+            {name}
+            <button onClick={() => handleDeleteTask(id)}>x</button>
+          </li>)
+        )}
       </ul>
     </div>
   );
